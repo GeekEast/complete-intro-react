e71043dd9aa5806b0fc1acebf0615a9798f070a6
ZenGeek <xiang.tan.p2109@gmail.com>
2020-04-15 00:25:06 +0930

    create Pet component and reuse it!

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
-const App = () => React.createElement(
-    "div",
-    {},
-    React.createElement(
-        "h1",
-        {},
-        "Adopt Me!"
-    )
-)
-ReactDOM.render(React.createElement(App), document.getElementById("root"))
+const Pet = () =>
+  React.createElement("div", {}, [
+    React.createElement("h1", {}, "Luna"), // name
+    React.createElement("h2", {}, "Dog"),  // type
+    React.createElement("h3", {}, "Havanese"), // breed
+  ]);
+
+
+const App = () =>
+  React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet),
+    React.createElement(Pet),
+    React.createElement(Pet),
+  ]);
+
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
