1e74d3796e36a0a3b2533a0abdf24aac7ee34613
ZenGeek <xiang.tan.p2109@gmail.com>
2020-04-15 00:39:27 +0930

    pass props to Pet to create custom differnt pets

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
-const Pet = () =>
+const Pet = (props) =>
   React.createElement("div", {}, [
-    React.createElement("h1", {}, "Luna"), // name
-    React.createElement("h2", {}, "Dog"),  // type
-    React.createElement("h3", {}, "Havanese"), // breed
+    React.createElement("h1", {}, props.name), // name
+    React.createElement("h2", {}, props.type), // type
+    React.createElement("h3", {}, props.breed), // breed
   ]);
-
 
 const App = () =>
   React.createElement("div", {}, [
     React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet),
-    React.createElement(Pet),
-    React.createElement(Pet),
+    React.createElement(Pet, { name: "Luna", type: "Dog", breed: "Havanese" }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      type: "Bird",
+      breed: "Cockatiel",
+    }),
+    React.createElement(Pet, { name: "Doink", type: "Cat", breed: "Mix" }),
   ]);
-
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
