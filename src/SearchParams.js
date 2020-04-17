6cfa00d89bc0782dc8019de5b2c0fc9f493dcd3e
ZenGeek <xiang.tan.p2109@gmail.com>
2020-04-17 20:07:03 +0930

    add type and breeds

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,7 +1,12 @@
 import React, { useState } from 'react';
+import { ANIMALS } from '@frontendmasters/pet';
 
 export default () => {
-  const [location, setLocation] = useState('Seattle, WA'); 
+  const [location, setLocation] = useState('Seattle, WA');
+  const [type, setType] = useState("");
+  const [breed, setBreed] = useState("");
+  const [breeds, setBreeds] = useState([]);
+
 
   return (
     <div className="search-params">
@@ -14,6 +19,37 @@ export default () => {
             placeholder="Location"
             onChange={(event) => setLocation(event.target.value)}
           />
+        </label>
+
+        <label htmlFor="type">
+          Animal
+          <select
+            id="type"
+            value={type}
+            onChange={(e) => setType(e.target.value)}
+            onBlur={e => setType(e.target.value)}
+          >
+              <option value="all">All</option>
+              {ANIMALS.map(t => (
+                  <option key={t} value={t}>{t}</option>
+              ))}
+          </select>
+        </label>
+
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+            onBlur={e => setBreed(e.target.value)}
+            disabled={!breeds.length}
+          >
+              <option value="all">All</option>
+              {breeds.map(b => (
+                  <option key={b} value={b}>{b}</option>
+              ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
